c96cd153464fa168c628ba043029680d2bededc4
Elliot Dubrow <emn2345@gmail.com>
2021-09-05 16:13:53 -0500

    formatting changes

--- discord_bot.py
+++ discord_bot.py
@@ -8,6 +8,7 @@ import functools
 import operator
 import imdb
 from imdb.Movie import Movie
+
 ia = imdb.IMDb()
 
 # connect to sqlite db file
@@ -18,16 +19,20 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 allowed_channel = 'movie-suggestions'
-help_command = commands.DefaultHelpCommand(
-    no_category='Commands'
-)
-bot = commands.Bot(command_prefix='!',
-                   description='A movie bot to help you make the hard decisions :)', help_command=help_command)
+help_command = commands.DefaultHelpCommand(no_category='Commands')
+bot = commands.Bot(
+    command_prefix='!',
+    description='A movie bot to help you make the hard decisions :)',
+    help_command=help_command)
 
-@bot.command(name='addmovie', help=': This command adds the movie of your choice to a database of movie choices, if the choice is already there it will not add it.')
+
+@bot.command(
+    name='addmovie',
+    help=
+    ': This command adds the movie of your choice to a database of movie choices, if the choice is already there it will not add it.'
+)
 async def add_movie(ctx, movie):
 
-    
     if ctx.channel.name == allowed_channel:
         # grab names from db as list
         rows = cursor.execute('SELECT name FROM movies').fetchall()
@@ -41,7 +46,7 @@ async def add_movie(ctx, movie):
                 response = 'You have logged {}'.format(movie)
 
         else:
-                # if no movies in db, this adds it
+            # if no movies in db, this adds it
             response = "Movie Added"
             insert_movie_sql(movie.lower())
     else:
@@ -49,7 +54,8 @@ async def add_movie(ctx, movie):
     await ctx.send(response)
 
 
-@bot.command(name='movies', help=': This command will show the current choices for movies')
+@bot.command(name='movies',
+             help=': This command will show the current choices for movies')
 async def check_movie_list(ctx):
     if ctx.channel.name == allowed_channel:
         rows = cursor.execute('SELECT name FROM movies').fetchall()
@@ -62,21 +68,21 @@ async def check_movie_list(ctx):
     await ctx.send(response)
 
 
-@bot.command(name='eraseall', help=': Only use this if you really need to erase everything')
+@bot.command(name='eraseall',
+             help=': Only use this if you really need to erase everything')
 async def erase_movies(ctx):
-      if ctx.channel.name == allowed_channel:
+    if ctx.channel.name == allowed_channel:
         cursor.execute('DELETE FROM movies WHERE id > 0')
         connection.commit()
-        response = "Database Erased."    
-      else:
+        response = "Database Erased."
+    else:
         response = "Please post commands in {} only".format(allowed_channel)
 
-      await ctx.send(response)
+    await ctx.send(response)
 
 
 def insert_movie_sql(movie):
-    
-    
+
     # create an insert statement to make it easier
     insert_movies_query = '''INSERT INTO movies (name) VALUES''' + \
         '(' + "'" + movie + "'" + ')'
@@ -86,7 +92,8 @@ def insert_movie_sql(movie):
     connection.commit()
 
 
-@bot.command(name='pick', help=': This command will pick a random movie from the list')
+@bot.command(name='pick',
+             help=': This command will pick a random movie from the list')
 async def pick_movie(ctx):
 
     rows = cursor.execute('SELECT name FROM movies').fetchall()
@@ -97,7 +104,7 @@ async def pick_movie(ctx):
         else:
             response = "Please post commands in movie-suggestions only."
     else:
-            response = "Database is empty, please add movies"
+        response = "Database is empty, please add movies"
     await ctx.send(response)
 
 
@@ -106,11 +113,10 @@ def pick_movie():
     random_choice = cursor.execute(
         'SELECT id FROM movies ORDER BY RANDOM() LIMIT 1').fetchone()
     st = functools.reduce(operator.add, random_choice)
-    movie_choice = cursor.execute(
-        'SELECT name FROM movies WHERE id = ' + str(st)).fetchone()
+    movie_choice = cursor.execute('SELECT name FROM movies WHERE id = ' +
+                                  str(st)).fetchone()
     mc = functools.reduce(operator.add, movie_choice)
 
-    
     imdb_movie = ia.search_movie(mc)
     imdb_movie_id = ia.get_movie(imdb_movie[0].getID())
     movie_url = ia.get_imdbURL(imdb_movie_id)
